12b9aa4bd29f3b971fc69d903aae64e039fa58e3
Qi <jackzong@qis-macbook-pro.lan>
2020-07-07 16:16:30 -0700

    change url1

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://34.70.230.107:9200"
+	ES_URL = "http://35.192.107.226:9200"
 )
 
 func main() {
